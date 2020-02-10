cf45e626d25da342d753704ada0ea3becb038200
Evgeniy Bastrykov <vporoshok@gmail.com>
2020-02-10 11:45:18 +0400

    up

---
+++ examples/07_insert_sort/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func sort(a []int) {
+	for i := 1; i < len(a); i++ {
+		j := i
+		for ; j > 0 && a[i] < a[j-1]; j-- {
+		}
+		for ; i > j; i-- {
+			a[i], a[i-1] = a[i-1], a[i]
+		}
+	}
+}
+
+func main() {
+	a := []int{12, 8, 22, 11, 1, 3}
+	sort(a)
+	fmt.Println(a) // [1 3 8 11 12 22]
+}
